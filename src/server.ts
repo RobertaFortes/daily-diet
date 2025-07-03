ad71b3a80dedc02752565747af94bd68d719f4f4
Roberta Fortes <betanickf@gmail.com>
2025-07-03 13:53:08 +0200

    fix: port connection

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import { env } from './env'
 
 app
   .listen({
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log('Server running on http://localhost:3333')
+    console.log('Server running on http://0.0.0.0:3333')
   })
   .catch((err) => {
     console.error('Error starting server:', err)
