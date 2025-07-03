6ffc4c23753cf8297beb3df993d409cb569b9227
Roberta Fortes <betanickf@gmail.com>
2025-07-03 13:33:54 +0200

    fix: db connection

--- src/database.ts
+++ src/database.ts
@@ -9,9 +9,7 @@ export const config: Knex.Config = {
   client: 'sqlite3',
   connection:
     env.DATABASE_CLIENT === 'sqlite'
-      ? {
-        filename: env.DATABASE_URL,
-      }
+      ? { filename: env.DATABASE_URL }
       : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
